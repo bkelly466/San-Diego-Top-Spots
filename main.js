58f29dc3fea7703a9bd12a8c5800f3a6bb5823a4
Brandon K <brandon@Brandons-MacBook-Air.local>
2026-05-05 12:50:20 -0400

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 $(document).ready(function () {
     // write your code here
+    $.getJSON("data.json", function(data){
+        $.each(data, function(index, value) {
+            let spots = $("#spots")
+            console.log(value.description);
+            let mapUrl = "https://www.google.com/maps?q=" + value.location;
 
+            let row = "<tr>" +
+                "<td>" + value.name + "</td>" +
+                "<td>" + value.description + "</td>" +
+                "<td>" + 
+                    "<a href='" + mapUrl + "' target='_blank' rel='noopener noreferrer'>" + 
+                        "Map" +
+                    "</a>" + 
+                "</td>" +
+                "</tr>";
+                
+            spots.append(row);
+        });
+    });
 });
